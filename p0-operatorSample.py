398cefd41b8113d9322b250187735e68d3779a85
BadeAnkita <99501430+BadeAnkita@users.noreply.github.com>
2022-02-19 10:21:20 +0530

    Basic Math operator

---
+++ p0-operatorSample.py
@@ -0,0 +1,25 @@
+# +,-,*,/,%,^,floor division
+a = 5
+b = 2
+add = a + b
+
+print("Addition =", add)
+
+sub = b - a
+print("Subtraction =", sub)
+
+mul = a * b
+print("Multiplication =", mul)
+
+div = b / a
+print("Division =", div)
+
+mod = a / b
+print("Module =", mod)
+
+expo = a ** b
+print("Exponential =", expo)
+
+flr_div = a // b
+print("Floor Division =", flr_div)
+
