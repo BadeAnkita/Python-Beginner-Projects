7c02f583f5bd3d752597d8a835a9a1ec81e14258
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-02-21 14:21:45 +0530

    Print using '

---
+++ p6-printConcat.py
@@ -0,0 +1,8 @@
+# simple print function in python
+# Type1 concatination or joining of string
+
+a = 10
+b = 30
+sum = a + b
+print('Sum of two number =' + str(sum))
+print('A=' + str(a) + 'B=' + str(b) + ' Sum=' + str(sum))
