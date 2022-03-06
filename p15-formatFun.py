1ce1cd3ee587a5b4014c55199d51b100efe5a4d0
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-06 19:50:49 +0530

    print output using format() Or format function
    here we use .format() inside print()
    .format() include variable which stores the value
    In this function placeholder { }  is also used

---
+++ p15-formatFun.py
@@ -0,0 +1,7 @@
+# Print() in python
+# Type 4- using format() Or format function
+
+ht = 12
+bs = 5
+areat = (bs / 2.0) * ht
+print('Height ={0}  \nBase ={1}  \nArea of Triangle ={2}'.format(ht, bs, areat))
