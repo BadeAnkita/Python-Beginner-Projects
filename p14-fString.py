2269746e63e70d7c499f9c9f501b7dda99d3f6ad
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-06 19:47:49 +0530

    Example of 'f' string

---
+++ p14-fString.py
@@ -0,0 +1,11 @@
+# print() type 3 f(string)
+
+rno = 56
+name = 'Ankita'
+phy = 60
+che = 90
+maths = 76
+gt = phy + che + maths # calculating grand total
+per = gt / 3.0  # calculating percentage
+
+print(f'Roll no= {rno}\nName= {name}\nGt= {gt}\nPer= {per}')
