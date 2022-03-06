db049c1081ac9c5953ef880f5abe734abab770b7
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-06 19:52:28 +0530

    Example of format() for output

---
+++ p16-formatFun.py
@@ -0,0 +1,12 @@
+# Print() in python
+# Type 4- using format() Or format function
+
+rno = 56
+name = 'Ankita'
+phy = 60
+che = 90
+maths = 76
+gt = phy + che + maths # calculating grand total
+per = gt / 3.0  # calculating percentage
+
+print('Roll no= {0}\nName= {1}\nGt= {2}\nPer= {3}'.format(rno, name, gt, per))
