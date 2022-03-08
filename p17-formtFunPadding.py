f58197fc304e4a836f7675d004f3509dd68cc27b
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-08 15:44:09 +0530

    Format() Function With Padding
    Print Output using format() function with also applying padding.
    Here we ':' symbol

---
+++ p17-formtFunPadding.py
@@ -0,0 +1,8 @@
+# print() with padding of data
+
+rno = 23
+name = 'Ankita'
+city = 'Gondia'
+per = 78
+print('Roll No={:20} \nName={:20} \nCity={:20} \nPercentage={:20}'.format(rno,name,city,per))
+
