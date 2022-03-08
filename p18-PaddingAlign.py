541623b2d4c3f7043387855d539d8d63eef792a8
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-08 15:50:21 +0530

    print() with padding and aligning of data
    To aligning and padding the data in python we use use format function
    It is used to print the output

---
+++ p18-PaddingAlign.py
@@ -0,0 +1,9 @@
+# print() with padding of data and align of data
+
+rno = 23
+name = 'Ankita'
+city = 'Gondia'
+per = 78
+print('Roll No={:20} \nName={:20} \nCity={:20} \nPercentage={:20}'.format(rno,name,city,per))
+print('Roll No={:>20} \nName={:>20} \nCity={:>20} \nPercentage={:>20}'.format(rno,name,city,per))
+print('Roll No={:<20} \nName={:<20} \nCity={:<20} \nPercentage={:<20}'.format(rno,name,city,per))
