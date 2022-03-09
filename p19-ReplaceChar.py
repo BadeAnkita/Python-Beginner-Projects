fb01aade67c4856050876fc9cff37312baad506f
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-09 17:16:54 +0530

    replacing blank spaces with special characters
    Print output using format() function, also print() with padding of data, align of data and replacing blank spaces with special characters

---
+++ p19-ReplaceChar.py
@@ -0,0 +1,7 @@
+# print() with padding of data, align of data and replacing blank spaces with special characters
+
+rno = 23
+name = 'Ankita'
+city = 'Gondia'
+per = 78
+print('Roll No={:*>20} \nName={:*<20} \nCity={:*>20} \nPercentage={:*<20}'.format(rno,name,city,per))
