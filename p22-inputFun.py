cf7257f320a27b8579fd68eb23ab84cd6b61190e
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-03-11 14:20:59 +0530

    Example of input()

---
+++ p22-inputFun.py
@@ -0,0 +1,6 @@
+# Input data using input() functio
+
+name = input('Enter your name:')
+city = input('Enter your city:')
+mail = input('Enter your mail:')
+print('\nName={:20s} \nCity={:20s} \nMail={:20s}'.format(name, city, mail))
