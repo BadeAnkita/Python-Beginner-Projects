9f725ef7d468a885399abaa7255bfe11ecf30503
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-04-06 18:09:38 +0530

    Control Statement (ODD/EVEN)
    Find number is even no  or odd no using if, else control statement

---
+++ p2-OddEve.py
@@ -0,0 +1,7 @@
+# Find number is even no  or odd no using control statement
+
+num = int(input('Enter Any number='))
+if num % 2 == 0:
+    print('{:d} is a Even number'.format(num))
+else:
+    print('{:d} is a odd number'.format(num))
