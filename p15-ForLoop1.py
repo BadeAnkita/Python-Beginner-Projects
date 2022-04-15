33500ffaa9433968cfea58dc135024aae19f9b35
Ankita Rajkumar Bade <99501430+BadeAnkita@users.noreply.github.com>
2022-04-15 12:52:14 +0530

    Control Statement(For loop)
    for loop as a sequence and collection(Tuple and List)

---
+++ p15-ForLoop1.py
@@ -0,0 +1,18 @@
+# for loop as a sequence and collection
+
+name = ['Ankita', 'Sakshi', 'Sagar', 'Lopi']
+n = (23, 45, 34, 78)
+per = [56.3, 89.23, 90.98, 78.60]
+student = ['Ankita', 23, 56.3]
+for a in name:
+    print(a, end=' ')
+print()
+for b in n:
+    print(b)
+print()
+for p in per:
+    print(p, end=' ')
+print()
+for s in student:
+    print(s, end=' ')
+    
